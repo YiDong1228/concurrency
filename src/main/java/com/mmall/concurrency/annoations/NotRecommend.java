76aef387cc4a77c094cc57aab0d022c218bc3695
yidong <yd1111@outlook.com>
2018-09-12 14:32:40 +0800

    'init'

---
+++ src/main/java/com/mmall/concurrency/annoations/NotRecommend.java
@@ -0,0 +1,22 @@
+package com.mmall.concurrency.annoations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @ProjectName: concurrency
+ * @Package: com.mmall.concurrency.annoations
+ * @ClassName: ThreadSafe
+ * @Description: 用来标记【不推荐】的类或写法
+ * @Author: yidong
+ * @CreateDate: 2018/9/12 14:24
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.SOURCE)
+public @interface NotRecommend {
+
+    String value() default "";
+
+}
